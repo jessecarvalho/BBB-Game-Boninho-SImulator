7f9d51e2b62ab187b4ed751f65fddb47b0c0e99b
jesseCarvalho <jesseelias80@gmail.com>
2020-05-03 15:46:50 -0300

    0.1.2
    - Leader prove improved
    - Names improved

--- data/actions/Prove.py
+++ data/actions/Prove.py
@@ -1,4 +1,5 @@
 from random import randint
+from time import sleep
 
 class prove:
     def __init__(self, participants):
@@ -11,16 +12,19 @@ class prove:
     def setParticipants(self, prove):
         for i in self.participants:
             if i.hability == prove:
-                i.life = 3
+                i.life = 2
             elif i.hability != prove:
                 i.life = 1
-            print(i.life, i.name)
 
     def event(self):
         while len(self.participants) > 1:
             random = randint(0, len(self.participants)-1)
-            self.participants[random].life = 0
+            self.participants[random].life -= 1
             if self.participants[random].life == 0:
+                print(f'O eliminado da rodada é: {self.participants[random].name}')
+                sleep(1)
                 self.participants.remove(self.participants[random])
-        print(self.participants[0].name)
+        winner = self.participants[0]
+        print(f'O vencedor é {winner.name}')
+        return winner
 
--- data/game.py
+++ data/game.py
@@ -19,7 +19,7 @@ class Game():
             print("-- Elenco criado com sucesso! --")
             print("-- 1 -> Gerar outro elenco --")
             print("-- 2 -> Ver elenco --")
-            print("-- 3 -> Avançar --")
+            print("-- 3 -> Avançar para primeira prova do lider --")
             decision = int(input("> "))
             if(decision == 1):
                 self.createCast()
@@ -33,20 +33,40 @@ class Game():
     def createCast(self):
         self.Cast.castGenerador()
 
+    def leaderProve(self):
+        leaderProve = Prove.prove(self.Cast.castList)
+        print('-- Escolha a prova dessa semana: --')
+        print('-- 1 - Arremesso de Dardos --')
+        print('-- 2 - Arremesso de bolas --')
+        print('-- 3 - Velocidade --')
+        print('-- 4 - Resistência --')
+        print('-- 5 - Corrida --')
+        print('-- 6 - Sorte --')
+        decision = int(input("> "))
+        if decision == 1:
+            prove = leaderProve.proveChoose("Arremesso de Dardos")
+        elif decision == 2:
+            prove = leaderProve.proveChoose("Arremesso de bolas")
+        elif decision == 3:
+            prove = leaderProve.proveChoose("Velocidade")
+        elif decision == 4:
+            prove = leaderProve.proveChoose("Resistência")
+        elif decision == 5:
+            prove = leaderProve.proveChoose("Corrida")
+        elif decision == 6:
+            prove = leaderProve.proveChoose("Sorte")
+        else:
+            print('Digito inválido')
+            self.leaderProve()
+        leaderProve.setParticipants(prove)
+        leaderProve.event()
+
     def showCast(self):
         self.Cast.show()
 
     def main(self):
         self.start()
-        game.prove()
-
-    def prove(self):
-        leaderProve = Prove.prove(self.Cast.castList)
-        prove = leaderProve.proveChoose("Sorte")
-        leaderProve.setParticipants(prove)
-        leaderProve.event()
-
-
+        game.leaderProve()
 
 
 if __name__ == '__main__':
