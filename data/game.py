ae883280b62f59ea233f9e70758c73f3b06d28f4
jesseCarvalho <jesseelias80@gmail.com>
2020-05-04 16:19:34 -0300

    0.1.3
    -Game improved

--- data/game.py
+++ data/game.py
@@ -33,9 +33,9 @@ class Game():
     def createCast(self):
         self.Cast.castGenerador()
 
-    def leaderProve(self):
-        leaderProve = Prove.prove(self.Cast.castList)
-        print('-- Escolha a prova dessa semana: --')
+    def actProve(self, proveType):
+        newProve = Prove.prove(self.Cast.castList)
+        print(f'-- Escolha a prova do {proveType} dessa semana: --')
         print('-- 1 - Arremesso de Dardos --')
         print('-- 2 - Arremesso de bolas --')
         print('-- 3 - Velocidade --')
@@ -44,29 +44,33 @@ class Game():
         print('-- 6 - Sorte --')
         decision = int(input("> "))
         if decision == 1:
-            prove = leaderProve.proveChoose("Arremesso de Dardos")
+            prove = newProve.proveChoose("Arremesso de Dardos")
         elif decision == 2:
-            prove = leaderProve.proveChoose("Arremesso de bolas")
+            prove = newProve.proveChoose("Arremesso de bolas")
         elif decision == 3:
-            prove = leaderProve.proveChoose("Velocidade")
+            prove = newProve.proveChoose("Velocidade")
         elif decision == 4:
-            prove = leaderProve.proveChoose("Resistência")
+            prove = newProve.proveChoose("Resistência")
         elif decision == 5:
-            prove = leaderProve.proveChoose("Corrida")
+            prove = newProve.proveChoose("Corrida")
         elif decision == 6:
-            prove = leaderProve.proveChoose("Sorte")
+            prove = newProve.proveChoose("Sorte")
         else:
             print('Digito inválido')
-            self.leaderProve()
-        leaderProve.setParticipants(prove)
-        leaderProve.event()
+            self.actProve(proveType)
+        newProve.setParticipants(prove)
+        return newProve.event().name
+
 
     def showCast(self):
         self.Cast.show()
 
     def main(self):
         self.start()
-        game.leaderProve()
+        leader = game.actProve("lider")
+        print(f'O lider da semana será: {leader}')
+        angel = game.actProve("anjo")
+        print(f'O anjo da semana será: {angel}')
 
 
 if __name__ == '__main__':
